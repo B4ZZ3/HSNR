fcd5858b3deb47f5afb9aa2952ba683aef43c52c
B4ZZ3 <develop.christel@gmail.com>
2022-04-13 19:40:00 +0200

    upd: member structure for task 3

--- DotNet/Uebung2/Program.cs
+++ DotNet/Uebung2/Program.cs
@@ -13,12 +13,29 @@
 //}
 
 //Aufgabe3
-struct Mitglied
-{
+//public struct Mitglied
+//{
+//    public String name { get; set; }
+//    public String vorname { get; set; }
+//    public DateOnly geburtsdatum { get; set; }
+//    public String abteilung { get; set; }
 
-}
+//    public Mitglied(String name, String vorname, DateOnly geburtsdatum, String abteilung)
+//    {
+//        this.name = name;
+//        this.vorname = vorname;
+//        this.geburtsdatum = geburtsdatum;
+//        this.abteilung = abteilung;
+//    }
+//}
+
+//var date = new DateOnly(1966, 11, 01);
+//Mitglied test1 = new Mitglied("Meier", "Walburga", date, "Handball");
 
 //Aufgabe4
-var date = new DateOnly(1966, 11, 01);
-Mitglied test1 = new Mitglied("Meier", "Walburga", date, "Handball");
-test1.drucken();
+//var date = new DateOnly(1966, 11, 01);
+//Mitglied test1 = new Mitglied("Meier", "Walburga", date, "Handball");
+//test1.drucken();
+
+//Aufgabe5 - Nim-Spiel
+
