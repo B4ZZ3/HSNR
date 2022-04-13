013fd6e4e6303d078800fac4cdc4f6386208e32d
B4ZZ3 <develop.christel@gmail.com>
2022-04-13 15:24:31 +0200

    add: first two exercises from the lecture

---
+++ DotNet/Uebung2/Mitglied.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2
+{
+    public class Mitglied
+    {
+        public String name { get; set; }
+        public String vorname { get; set; }
+        public DateOnly geburtsdatum { get; set; }
+        public String abteilung { get; set; }
+
+        public Mitglied(String name, String vorname, DateOnly geburtsdatum, String abteilung)
+        {
+            this.name = name;
+            this.vorname = vorname;
+            this.geburtsdatum = geburtsdatum;
+            this.abteilung = abteilung;
+        }
+
+        public void drucken()
+        {
+            Console.WriteLine(this.name + " " + this.vorname + " " + this.geburtsdatum + " " + this.abteilung);
+        }
+    }
+}
---
+++ DotNet/Uebung2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Uebung2;
+
+//Aufgabe2
+//int i, k;
+//for (i = 1; i < 4; i = i + 3)
+//{
+//    k = 1;
+//    while (k < 4)
+//    {
+//        k = k + 3;
+//        Console.WriteLine(k + "\t" + i);
+//    }
+//}
+
+//Aufgabe3
+struct Mitglied
+{
+
+}
+
+//Aufgabe4
+var date = new DateOnly(1966, 11, 01);
+Mitglied test1 = new Mitglied("Meier", "Walburga", date, "Handball");
+test1.drucken();
