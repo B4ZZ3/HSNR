853072dd0ceca63fafd68178c1ab8eea7d1a8fd2
B4ZZ3 <develop.christel@gmail.com>
2022-04-20 17:55:45 +0200

    upd: enhancement of map code

--- DotNet/Uebung3/Program.cs
+++ DotNet/Uebung3/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
+using System.Collections.Generic;
+
+//personal path for your code
+String path = "C:\\Users\\Bjarne\\Documents\\HSNR\\HSNR-Code\\DotNet\\Uebung3\\";
 
 //Circle Aufgabe1
 //Bitmap b1 = new Bitmap(255, 255);
@@ -10,37 +15,43 @@ using System.Drawing.Imaging;
 //g1.DrawLine(myPen1, x1, y1, x2, y2);
 //Pen myPen2 = new Pen(Color.ForestGreen, 5);
 //g1.DrawEllipse(myPen2, (127 - 50), (127 - 50), 100, 100);
-//b1.Save(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\Aufgabe3-circle.jpg", ImageFormat.Jpeg);
+//b1.Save(@path + "Aufgabe3-circle.jpg", ImageFormat.Jpeg);
 
 //Aufgabe2 Karte zusammenbauen
-Image img1 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8484_5470.png");
-Image img2 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8484_5471.png");
-Image img3 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8484_5472.png");
-Image img4 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8485_5470.png");
-Image img5 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8485_5471.png");
-Image img6 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8485_5472.png");
-Image img7 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8486_5470.png");
-Image img8 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8486_5471.png");
-Image img9 = Image.FromFile(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\kacheln\14_8486_5472.png");
+var images = new List<Bitmap>();
+var files = Directory.GetFiles(path + "kacheln");
+var posX = 0; 
+var posY = 0;
+var step = 256;
+var imagePos = 0;
 
+//Load all files from directory
+for (int i = 0; i < files.Count(); i++)
+{
+    String nextImage = files[i];
+    Bitmap bitmap = Bitmap.FromFile(nextImage) as Bitmap;
+    images.Add(bitmap);
+}
+
+//create bitmap to draw on
 Bitmap karte = new Bitmap(768, 768);
 Graphics g = Graphics.FromImage(karte);
 g.Clear(Color.White);
 
-g.DrawImage(img1, 0, 0);
-g.DrawImage(img2, 0, 256);
-g.DrawImage(img3, 0, 512);
 
-g.DrawImage(img4, 256, 0);
-g.DrawImage(img5, 256, 256);
-g.DrawImage(img6, 256, 512);
-
-g.DrawImage(img7, 512, 0);
-g.DrawImage(img8, 512, 256);
-g.DrawImage(img9, 512, 512);
+//draw all 9 images on correct position
+for(int i = 0; i < 3; i++)
+{
+    posY = 0;
+    for(int j = 0; j < 3; j++)
+    {
+        g.DrawImage(images[imagePos], posX, posY);
+        imagePos++;
+        posY += step;
+    }
+    posX += step;
+}
 
 g.Dispose();
 
-karte.Save(@"C:\Users\Bjarne\Documents\HSNR\HSNR-Code\DotNet\Uebung3\karte.jpg", ImageFormat.Jpeg);
-
-//Ende
+karte.Save(@path + "karte.jpg", ImageFormat.Jpeg);
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Code from my university for my bachelor's degree.
 Each class is represented where I wrote some code
 
-##Overview for each class:
+## Overview for each class:
 1. [OOP1](/OOP1)
 2. [OOP2](/OOP2)
 3. [Algo](/Algo)
